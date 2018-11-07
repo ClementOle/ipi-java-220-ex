3668161e6e01017f80394ff253f99e11ee798cb4
Clément Olewski <clement.olewski@ipilyon.net>
2018-11-07 16:06:31 +0100

    Ajout Main

---
+++ src/test/java/com/ipiecoles/java/java220/Main.java
@@ -0,0 +1,18 @@
+package com.ipiecoles.java.java220;
+
+import org.joda.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        //Employe e = new Employe();
+       // Employe e2 = new Employe();
+        /*try {
+            e.setDateEmbauche(new LocalDate(2019, 2, 5));
+        } catch (Exception e1){
+            System.out.println(e1.getMessage());
+        }*/
+        //System.out.println(e.toString());
+        //System.out.println(e.hashCode());
+        //System.out.println(e.equals(e2));
+    }
+}
